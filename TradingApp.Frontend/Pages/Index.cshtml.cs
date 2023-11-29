6ac686d2bf9ddd8c5673e3f67dd91fe36a7c4315
Kyriakos <kyriakos.bakirlis@gmail.com>
2023-11-29 19:43:01 +0100

    TradingApp: Adding wisdom of the day

--- TradingApp.Frontend/Pages/Index.cshtml.cs
+++ TradingApp.Frontend/Pages/Index.cshtml.cs
@@ -6,11 +6,15 @@ namespace TradingApp.Frontend.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private readonly IPhraseProvider _phraseProvider;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(IPhraseProvider phraseProvider, ILogger<IndexModel> logger)
     {
         _logger = logger;
+        _phraseProvider = phraseProvider;
     }
+    
+    public string GetRandomPhrase() => _phraseProvider.GetPhrase();
 
     public void OnGet()
     {
--- TradingApp.Frontend/Program.cs
+++ TradingApp.Frontend/Program.cs
@@ -1,7 +1,10 @@
+using TradingApp.Frontend;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddScoped<IPhraseProvider, PhraseProvider>();
 
 var app = builder.Build();
 
